3de973dcef103e4900bd9551bf552d2d2748223b
PinkFished <d.macias.apps@gmail.com>
2020-04-30 21:44:14 -0700

    Updated file structure

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const apiRouter = require("./controller/api")
-const htmlRouter = require("./controller/routes")
-
 
 const PORT = process.env.PORT || 3000
 const app = express();
@@ -14,14 +12,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/secretDB", {
   useCreateIndex: true,
 });
 
-
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
 app.use(apiRouter);
-app.use(htmlRouter);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`)
