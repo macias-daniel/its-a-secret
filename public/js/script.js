a1d7f6e12e3296638a47350d5bf04f5a157ff983
PinkFished <d.macias.apps@gmail.com>
2020-05-02 00:53:27 -0700

    Added toggle visibility functionlity

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,17 @@
+const secretTextBox = $("#secretText")
+const categorySelect = $("#categorySelect")
+const postSecretButton = $(".post")
+const switchViewButton = $(".switch-view")
+let isMainView = true
 
 $(() => {
-  $(".post").on("click", (event) => {
+
+  //Post secret button
+  postSecretButton.on("click", (event) => {
     event.preventDefault()
-    const secretText = $("#secretText").val().trim()
-    const category = $("#categorySelect").val()
+
+    const secretText = secretTextBox.val().trim()
+    const category = categorySelect.val()
 
     //Check if fields are empty
     if (category === "notValid" || secretText === "") {
@@ -18,14 +26,32 @@ $(() => {
     postNewSecret(newSecret)
 
     //Clear text and category fields
-    $("#secretText").val("")
-    $("#categorySelect").val("notValid")
+    secretTextBox.val("")
+    categorySelect.val("notValid")
 
     console.log("Posted")
   })
+
+
+  //Switch view button
+  switchViewButton.on("click", (event => {
+    event.preventDefault()
+    switchView()
+
+    if (isMainView === true) {
+      switchViewButton.text("Post a Secret")
+      isMainView = false
+    } else {
+      switchViewButton.text("View Secrets")
+      isMainView = true
+    }
+
+  }))
 });
 
-
+function switchView() {
+  $(".new-post-container").toggle()
+}
 
 function postNewSecret(newSecret) {
   try {
