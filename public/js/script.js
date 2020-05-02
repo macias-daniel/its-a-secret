9637d1d8a7289d19f08ed5abeb0f4d2e62194a2e
PinkFished <d.macias.apps@gmail.com>
2020-05-01 21:41:52 -0700

    Basic post functionality and framework changed to bootstrap

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,42 @@
 $(() => {
   $(".post").on("click", (event) => {
     event.preventDefault()
-    getAll()
+    const secretText = $("#secretText").val().trim()
+    const category = $("#categorySelect").val()
 
+    //Check if fields are empty
+    if (category === "notValid" || secretText === "") {
+      return console.log("Invalid input")
+    }
+
+    const newSecret = {
+      secretContent: secretText,
+      category: category
+    }
+
+    postNewSecret(newSecret)
+
+    //Clear text and category fields
+    $("#secretText").val("")
+    $("#categorySelect").val("notValid")
+
+    console.log("Posted")
   })
 });
+
+
+
+function postNewSecret(newSecret) {
+  try {
+    fetch("/api/secret", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newSecret)
+    })
+
+  } catch (err) {
+    console.log(err)
+  }
+}
